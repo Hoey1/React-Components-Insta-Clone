bb468302b329478568e134cb3c4762edd5cefb8c
Joey <joey@xm8.ninja>
2020-05-12 18:26:14 -0400

    I dont even know

--- src/components/SearchBar/SearchBarContainer.js
+++ src/components/SearchBar/SearchBarContainer.js
@@ -1,21 +1,27 @@
 // You do not need to change any code in this file
 import React from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faInstagram } from '@fortawesome/free-brands-svg-icons'
-import { faHeart, faCircle, faCompass } from '@fortawesome/free-regular-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+import {
+  faHeart,
+  faCircle,
+  faCompass,
+} from "@fortawesome/free-regular-svg-icons";
 import "./SearchBar.css";
 
-const SearchBar = () => {
+const SearchBar = (props) => {
+  const changeHandler = (event) => {
+    const searchTerm = event.target.value;
+    props.setSearchTerm(searchTerm);
+  };
+
   return (
     <div className="search-bar-wrapper">
       <div className="social">
         <FontAwesomeIcon icon={faInstagram} />
       </div>
       <form className="search-form">
-        <input
-          type="text"
-          placeholder="Search"
-        />
+        <input onChange={changeHandler} type="text" placeholder="Search" />
       </form>
       <div className="social-wrapper">
         <div className="social">

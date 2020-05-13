2e4d3207bda354cfaf5baec5c41ac4e2e98286e6
Joey <joey@xm8.ninja>
2020-05-12 21:55:55 -0400

    FINALLY GOT IT - WITH A LOT OF HELP - A LOT

--- src/components/CommentSection/CommentSectionContainer.js
+++ src/components/CommentSection/CommentSectionContainer.js
@@ -14,7 +14,6 @@ const CommentSection = (props) => {
       {commentObj.map((comment) => {
         return <Comment comment={comment} />;
       })}
-
       <CommentInput />
     </div>
   );
--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -1,15 +1,21 @@
 //Complete the necessary code in this file
 // import useState
-import React from "react";
+import React, { useState } from "react";
 import Post from "./Post";
 import "./Posts.css";
-// import data 
 
-const PostsPage = () => {
+// import data
+
+const PostsPage = (props) => {
   // set up state for your data
+  const [posts, setPosts] = useState(props.filteredData);
+
   return (
     <div className="posts-container-wrapper">
       {/* map through data here to return a Post and pass data as props to Post */}
+      {props.filteredData.map((post) => {
+        return <Post post={post} />;
+      })}
     </div>
   );
 };
